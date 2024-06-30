f0bf1bf4d0fd3a279df20c72821812381514ca9c
Brent <brentverstreepen@hotmail.com>
2024-06-30 14:05:47 +0200

    update BankProgram.py

---
+++ BankProgram.py
@@ -0,0 +1,20 @@
+# Bank program
+# Create variables
+
+def showBalance():
+    pass
+
+def deposit():
+    pass
+
+def withdraw():
+    pass
+
+balance = 0
+is_running = True
+
+while is_running:
+
+
+def main():
+    pass
