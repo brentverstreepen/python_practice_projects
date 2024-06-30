fff995e9cd45db15b810ee135170912887d7e6e1
Brent <brentverstreepen@hotmail.com>
2024-06-30 13:01:07 +0200

    update CompoundInterestCalculator.py

--- CompoundInterestCalculator.py
+++ CompoundInterestCalculator.py
@@ -7,27 +7,34 @@ time = 0
 # Create loop to ask for user input and place it in variables
 while principle <= 0:
     try:
+        # Convert input to float
         principle = float(input("Enter the principle amount: "))
+        # Print error message if value is <= 0
         if principle <= 0:
-            print("Principle amount can't be less than or equal to zero.")
-
+            print("Principle amount can't be less than or equal to zero")
+    # Print error message if user gives wrong input
     except ValueError:
-        print("Please enter a number.")
+        print("Please enter a number")
 
 while rate <= 0:
     try:
+        # Convert input to float
         rate = float(input("Enter the rate: "))
+        # Print error message if value is <= 0
         if rate <= 0:
-            print("Rate can't be less than or equal to zero.")
+            print("Rate can't be less than or equal to zero")
 
     except ValueError:
         print("Please enter a number")
 
 while time <= 0:
     try:
+        # Convert input to float
         time = float(input("Enter the time in years: "))
+        # Print error message if value is <= 0
         if time <= 0:
-            print("Time can't be less than or equal to zero.")
+            print("Time can't be less than or equal to zero")
+    # Print error message if user gives wrong input
     except ValueError:
         print("Please enter a number")
 
