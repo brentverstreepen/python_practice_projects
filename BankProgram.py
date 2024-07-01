8d77e47ca709be7538987f5d9c9867ab6f3c8bbb
brentverstreepen <brentverstreepen@hotmail.com>
2024-07-01 21:20:22 +0200

    update project BankProgram.py

--- BankProgram.py
+++ BankProgram.py
@@ -1,5 +1,4 @@
 # Bank program
-# Create variables
 
 def showBalance():
     pass
@@ -13,8 +12,31 @@ def withdraw():
 balance = 0
 is_running = True
 
+# Create options for user
 while is_running:
+    print("Banking Program")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
 
+    # Ask for user input
+    choice = input("Enter your choice (1-4): ")
+
+    # Use input to run the chosen choice
+    if choice == "1":
+        showBalance()
+    elif choice == "2":
+        deposit()
+    elif choice == "3":
+        withdraw()
+    elif choice == "4":
+        is_running = False
+    else:
+        print("Please enter a valid number\n")
+
+# Print goodbye message if user exits
+print("Goodbye")
 
 def main():
     pass
