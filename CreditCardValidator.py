308c415f5ef89f20957363731f23afe674734e6c
brentverstreepen <brentverstreepen@hotmail.com>
2024-07-15 21:26:20 +0200

    update project CreditCardValidator.py

--- CreditCardValidator.py
+++ CreditCardValidator.py
@@ -16,22 +16,31 @@ card_number = input("Enter you card number: ")
 # Remove "-" and " "
 card_number = card_number.replace("-", "")
 card_number = card_number.replace(" ", "")
-print(card_number)
 
 # Step 2
 # Reverse number
 card_number = card_number[::-1]
-print(card_number)
-# Add digits in odd places -> 1, 3, 5...
+# Sum of digits in odd places -> 1, 3, 5...
 for x in card_number[::2]:
     sum_odd_digits += int(x)
 
 # Step 3
+# Start with 2nd number, step of 2
 for x in card_number[1::2]:
+    # Double it
     x = int(x) * 2
+    # add numbers together if double-digit
     if x >= 10:
         sum_even_digits += (1 + (x % 10))
     else:
         sum_even_digits += x
 
 # Step 4
+total = sum_odd_digits + sum_even_digits
+
+# Step 5
+# If divisible by 10
+if total % 10 == 0:
+    print(f"Valid Credit Card")
+else:
+    print(f"Invalid Credit Card")
